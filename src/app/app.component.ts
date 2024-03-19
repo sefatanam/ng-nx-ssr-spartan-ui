2809ed62fadc0ed56f8fb022e4c136273144dcd1
sefat.anam <msefat@saastaq.io>
2024-03-20 02:11:34 +0600

    initial: angular nx workspace with spartan ui setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HlmAccordionContentDirective, HlmAccordionDirective, HlmAccordionIconDirective, HlmAccordionItemDirective, HlmAccordionModule, HlmAccordionTriggerDirective } from '@spartan-ng/ui-accordion-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { BrnAccordionContentComponent } from '@spartan-ng/ui-accordion-brain';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, BrnAccordionContentComponent,
+    HlmAccordionDirective,
+    HlmAccordionItemDirective,
+    HlmAccordionTriggerDirective,
+    HlmAccordionContentDirective,
+    HlmAccordionIconDirective,
+    HlmIconComponent,],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
