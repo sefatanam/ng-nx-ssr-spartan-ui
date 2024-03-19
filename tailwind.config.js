d16931d64c5e81572b2d316c2f02158f5012d6f1
sefat.anam <msefat@saastaq.io>
2024-03-20 03:02:01 +0600

    fix: theme rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:'class' ,
   presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
